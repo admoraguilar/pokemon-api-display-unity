dda200d68e89593b0c279f229a2a89c251713a27
Admor Aguilar <admor.aguilar@gmail.com>
2023-05-13 12:27:24 +0800

    + Initial commit

---
+++ Assets/PokemonAPIDisplay/PokemonDisplay/PokemonDisplay.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using TMPro;
+
+namespace PokemonDisplay
+{
+	public class PokemonDisplay : MonoBehaviour
+	{
+		public List<string> pokemons = new List<string>();
+		public int pokemonIndex;
+
+		[SerializeField]
+		private RawImage _rawImage;
+
+		[SerializeField]
+		private TMP_Text _nameText;
+
+		[SerializeField]
+		private TMP_Text _expText;
+
+		[SerializeField]
+		private TMP_Text _weightText;
+
+		[SerializeField]
+		private TMP_Text _typeText;
+
+		[SerializeField]
+		private Button _prevButton;
+
+		[SerializeField]
+		private Button _nextButton;
+
+		private IEnumerator GetPokemonRoutine(string name)
+		{
+			// Get info
+			string pokeApiUrl = "https://pokeapi.co/api/v2/pokemon";
+			string fullPokeApiUrl = $"{pokeApiUrl}/{name}";
+			
+			UnityWebRequest pokeApiUrlGet = UnityWebRequest.Get(fullPokeApiUrl);
+			yield return pokeApiUrlGet.SendWebRequest();
+
+			if(pokeApiUrlGet.result == UnityWebRequest.Result.ConnectionError || 
+				pokeApiUrlGet.result == UnityWebRequest.Result.DataProcessingError) {
+				yield break;
+			}
+
+			dynamic pokeInfo = JsonConvert.DeserializeObject<dynamic>(
+				pokeApiUrlGet.downloadHandler.text);
+
+			// Get sprite
+			string pokeSpriteURL = pokeInfo["sprites"]["front_default"];
+
+			UnityWebRequest pokeApiSpriteGet = UnityWebRequestTexture.GetTexture(pokeSpriteURL);
+			yield return pokeApiSpriteGet.SendWebRequest();
+
+			if(pokeApiSpriteGet.result == UnityWebRequest.Result.ConnectionError ||
+				pokeApiSpriteGet.result == UnityWebRequest.Result.DataProcessingError) {
+				yield break;
+			}
+
+			// Set all details
+			string pokeName = pokeInfo["name"];
+			string pokeEXP = pokeInfo["base_experience"];
+			string pokeWeight = pokeInfo["weight"];
+			string pokeType = pokeInfo["types"][0]["type"]["name"];
+
+			_nameText.text = $"Name: {pokeName}";
+			_expText.text = $"EXP: {pokeEXP}";
+			_weightText.text = $"Weight: {pokeWeight}";
+			_typeText.text = $"Type: {pokeType}";
+			_rawImage.texture = DownloadHandlerTexture.GetContent(pokeApiSpriteGet);
+			_rawImage.texture.filterMode = FilterMode.Point;
+		}
+
+		private void OnPrevButtonClicked()
+		{
+			if(--pokemonIndex < 0) { pokemonIndex = pokemons.Count - 1; }
+			StartCoroutine(GetPokemonRoutine(pokemons[pokemonIndex]));
+		}
+
+		private void OnNextButtonClicked()
+		{
+			if(++pokemonIndex >= pokemons.Count) { pokemonIndex = 0; }
+			StartCoroutine(GetPokemonRoutine(pokemons[pokemonIndex]));
+		}
+
+		private void Start()
+		{
+			StartCoroutine(GetPokemonRoutine(pokemons[pokemonIndex]));
+		}
+
+		private void OnEnable()
+		{
+			_prevButton.onClick.AddListener(OnPrevButtonClicked);
+			_nextButton.onClick.AddListener(OnNextButtonClicked);
+		}
+
+		private void OnDisable()
+		{
+			_prevButton.onClick.RemoveListener(OnPrevButtonClicked);
+			_nextButton.onClick.RemoveListener(OnNextButtonClicked);
+		}
+	}
+}
